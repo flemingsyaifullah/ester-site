43f5f005b027eaa57c5cb717efd9a0363968623e
flemingsyaifullah <flemingsyaifullah@gmail.com>
2026-07-05 14:26:22 +0700

    feat(ester-site): update seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://globalindoperizinan.id",
+  site: "https://www.ester-globalindo.com",
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
